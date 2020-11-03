c52f2fde9af22c74f9be07d3bfdbf533daafa906
Wendi Freeman <wfreeman2112@gmail.com>
2020-11-03 14:00:21 -0600

    preliminary HTML and CSS

---
+++ script.js
@@ -0,0 +1,43 @@
+/*make form inputs
+    -input city 
+take weather info from input field
+    -city name
+    -date
+    -icon 
+    -temp
+    -humidity
+    -windspeed 
+    -uv
+-click on UV index
+    -turn color that represents severity
+-future weather conditions button
+    -5 day forecast
+-click on city in search history
+    -presented with past and future
+-when opened presented with last search city
+*/
+
+
+
+
+
+$(document).ready(function() {
+    
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
