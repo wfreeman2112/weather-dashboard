c48a82b2bc6c1161e45509c059a8e5cfae6b7b83
Wendi Freeman <wfreeman2112@gmail.com>
2020-11-04 14:33:21 -0600

    retrieving info in JS

--- script.js
+++ script.js
@@ -20,9 +20,60 @@ take weather info from input field
 
 
 
-
 $(document).ready(function() {
     
+    $('#weather-form').submit(function(e) {
+        e.preventDefault()
+        var city = $('#weather-input').val();
+        $.ajax({
+            url: 'https://api.openweathermap.org/data/2.5/weather?q='+ city + '&appid=6d33391dce0cc9321d1501a34aba3f3d&units=imperial',
+            type: "GET",
+            dataType: "json",
+            success: function (data) {
+                console.log(data);
+
+             //getting UV AJAX call
+             
+             /*
+            
+             $.ajax({
+                url: 'http://api.openweathermap.org/data/2.5/uvi?lat=' + data.coord.lat + 'lon=' + data.coord.lon + ' + &appid=6d33391dce0cc9321d1501a34aba3f3d',
+                type: "GET",
+                dataType: "json",
+            
+        
+             });
+             
+             */
+            
+             //getting and displaying info 
+            $('#weather-info').html('');
+           $("#weather-info").append('<p>' + city + '</p>');
+           $("#weather-info").append('<p>' + moment.unix(data.dt).format('MMMM DD, YYYY') + '</p>');
+           $("#weather-info").append('<p>' + data.main.temp + '</p>');
+           $("#weather-info").append('<p>' + data.main.humidity + '</p>');
+           $("#weather-info").append('<p>' + data.wind.speed + '</p>');
+           $("#weather-info").append('<img src = "http://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png">');
+
+            },
+            error: function (error) {
+                console.log(`Error ${error}`);
+            }
+        });
+    });
+    
+    
+
+
+    //storing input info
+    //click event for UV index
+    //make UV call
+    //make 5 day forecast call
+    
+    
+    
+
+
     });
 
 
@@ -40,4 +91,3 @@ $(document).ready(function() {
 
 
 
-});
