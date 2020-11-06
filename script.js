2e2fabca85a3b92975aa623c96a662f7ffbef9bd
Wendi Freeman <wfreeman2112@gmail.com>
2020-11-06 10:48:45 -0600

    more JS AJAX calls

--- script.js
+++ script.js
@@ -21,6 +21,9 @@ take weather info from input field
 
 
 $(document).ready(function() {
+
+    
+    //AJAX call for weather info
     
     $('#weather-form').submit(function(e) {
         e.preventDefault()
@@ -29,65 +32,56 @@ $(document).ready(function() {
             url: 'https://api.openweathermap.org/data/2.5/weather?q='+ city + '&appid=6d33391dce0cc9321d1501a34aba3f3d&units=imperial',
             type: "GET",
             dataType: "json",
-            success: function (data) {
-                console.log(data);
+            success: firstWeatherSuccess
+               
 
-             //getting UV AJAX call
-             
-             /*
-            
-             $.ajax({
-                url: 'http://api.openweathermap.org/data/2.5/uvi?lat=' + data.coord.lat + 'lon=' + data.coord.lon + ' + &appid=6d33391dce0cc9321d1501a34aba3f3d',
-                type: "GET",
-                dataType: "json",
-            
-        
-             });
-             
-             */
-            
-             //getting and displaying info 
-            $('#weather-info').html('');
-           $("#weather-info").append('<p>' + city + '</p>');
-           $("#weather-info").append('<p>' + moment.unix(data.dt).format('MMMM DD, YYYY') + '</p>');
-           $("#weather-info").append('<p>' + data.main.temp + '</p>');
-           $("#weather-info").append('<p>' + data.main.humidity + '</p>');
-           $("#weather-info").append('<p>' + data.wind.speed + '</p>');
-           $("#weather-info").append('<img src = "http://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png">');
-
-            },
-            error: function (error) {
-                console.log(`Error ${error}`);
-            }
+         // error: function (error) {
+         // console.log(`Error ${error}`);
+         // };
+     
+     
+           
+    
         });
-    });
     
     
-
-
-    //storing input info
-    //click event for UV index
-    //make UV call
-    //make 5 day forecast call
-    
-    
-    
-
-
     });
 
+//make 5 day call
+});
 
+function firstWeatherSuccess(data){
 
+        console.log(data);
 
+        //getting UV AJAX call
+     
+     
+    
+         var lat = data.coord.lat;
+         var lon = data.coord.lon;
+        $.ajax({
+            url: 'https://api.openweathermap.org/data/2.5/uvi?&lat=' + lat + '&lon=' + lon + '&appid=6d33391dce0cc9321d1501a34aba3f3d',
+             type: 'GET',
+             dataType: "json",
+            success: function (data) {
+            console.log(data);
+            console.log(data.value);
+             }
+         });
 
+     
+    
+        //  displaying info 
 
-
-
-
-
-
-
-
-
-
-
+        $('#weather-info').html('');
+        //$("#weather-info").append('<p>' + city + '</p>');
+        $("#weather-info").append('<p>' + moment.unix(data.dt).format('MMMM DD, YYYY') + '</p>');
+        $("#weather-info").append('<p>' + data.main.temp + " degrees F" + '</p>');
+        $("#weather-info").append('<p>' + data.main.humidity + "% humidity" + '</p>');
+        $("#weather-info").append('<p>' + data.wind.speed + '</p>');
+        //$("#weather-info").append('<p>'  + data.value + '</p>');
+        $("#weather-info").append('<img src = "http://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png">');
+   
+    
+}
